9ce932c097cf6d01785d094a8de40e602791271c
Chiragpatil05 <itschiragpatil5@gmail.com>
2024-08-15 21:38:10 +0530

    backend setup and schema created

---
+++ server/controllers/user.js
@@ -0,0 +1,5 @@
+const login = (req , res) => {
+    res.send("this is user login route page")
+}
+
+export {login}
---
+++ server/models/message.js
@@ -0,0 +1,32 @@
+import { Schema , model , models , Types } from "mongoose";
+
+const schema = new Schema({
+    content:{
+        type:String,
+    },
+    sender:{
+        type: Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    chat:{
+        type: Types.ObjectId,
+        ref:"Chat",
+        required:true,
+    },
+    attachments:[{
+        public_id:{
+            type:String,
+            required:true,
+        },
+        url:{
+            type:String,
+            required:true,
+        }
+    }]
+
+} , {
+    timestamps:true,
+});
+
+export const Message = models.Message || model("Message",schema);
